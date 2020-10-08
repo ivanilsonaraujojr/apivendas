6e61c38e628076c1cf0a785a6413902490d6181b
Ivanilson Junior <ivanilsonaraujojr@gmail.com>
2020-10-08 14:37:45 -0300

    Adicionando Form da entidade anuncio

---
+++ src/main/java/br/com/ivanilsonjr/controller/form/AnuncioForm.java
@@ -0,0 +1,50 @@
+package br.com.ivanilsonjr.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import br.com.ivanilsonjr.model.Anuncio;
+import br.com.ivanilsonjr.repository.ProdutoRepository;
+import br.com.ivanilsonjr.repository.UsuarioRepository;
+
+public class AnuncioForm {
+	@NotEmpty @Size(min = 7, message="Titulo do anuncio invalido")
+	private String titulo;
+	@NotEmpty @Size(min = 20, message="Descrição do anuncio invalida")
+	private String descricao;
+	@NotNull
+	private Long codigoProduto;
+	@NotNull
+	private Double preco;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Long getCodigoProduto() {
+		return codigoProduto;
+	}
+	public void setCodigoProduto(Long codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public Anuncio converter(ProdutoRepository produtoRepository, UsuarioRepository usuarioRepository) {
+		return new Anuncio(titulo, descricao, produtoRepository.find(codigoProduto, Long.parseLong("1")), preco, 
+				usuarioRepository.findById(Long.parseLong("1")).get());
+	}
+	
+}
