17391d73c5fd8e1cc548327e4b143582fbf4a3b7
Ivanilson Junior <ivanilsonaraujojr@gmail.com>
2020-10-08 16:56:25 -0300

    Adicionando metodo de atualização de anuncios e refatorando codigo

--- src/main/java/br/com/ivanilsonjr/controller/AnuncioController.java
+++ src/main/java/br/com/ivanilsonjr/controller/AnuncioController.java
@@ -3,6 +3,7 @@ package br.com.ivanilsonjr.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.ivanilsonjr.controller.dto.AnuncioDto;
 import br.com.ivanilsonjr.controller.form.AnuncioForm;
-import br.com.ivanilsonjr.model.Anuncio;
+import br.com.ivanilsonjr.controller.form.AtualizacaoAnuncioForm;
 import br.com.ivanilsonjr.service.AnuncioService;
 
 @RestController
@@ -47,11 +49,18 @@ public class AnuncioController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@PutMapping("/{codigo}")
+	@Transactional
+	public ResponseEntity<AnuncioDto> atualizarAnuncio(@PathVariable Long codigo, @Valid @RequestBody AtualizacaoAnuncioForm atualizacaoAnuncioForm){
+		AnuncioDto dto = as.atualizarAnuncio(codigo, atualizacaoAnuncioForm);
+		return ResponseEntity.ok(dto);
+	}
+
 	@PostMapping
 	public ResponseEntity<AnuncioDto> cadastrarAnuncio(@Valid @RequestBody AnuncioForm anuncioForm, UriComponentsBuilder uriBuilder){
-		Anuncio anuncio = as.cadastrarAnuncio(anuncioForm);
-		URI uri = uriBuilder.path("/anuncio/{id}").buildAndExpand(anuncio.getCodigo()).toUri();
-		return ResponseEntity.created(uri).body(new AnuncioDto(anuncio));
+		AnuncioDto dto = as.cadastrarAnuncio(anuncioForm);
+		URI uri = uriBuilder.path("/anuncio/{id}").buildAndExpand(dto.getCodigo()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@DeleteMapping("/{codigo}")
