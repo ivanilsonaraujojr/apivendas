e61437f6dab3e3580cc1e806a0c0b181ed5aed1d
Ivanilson Junior <ivanilsonaraujojr@gmail.com>
2020-10-08 15:27:50 -0300

    Adicionando Dto para exceptions genericas

---
+++ src/main/java/br/com/ivanilsonjr/config/validacao/ErroGenericoDto.java
@@ -0,0 +1,15 @@
+package br.com.ivanilsonjr.config.validacao;
+
+public class ErroGenericoDto {
+
+	private String erro;
+	
+	public ErroGenericoDto(String erro) {
+		this.erro = erro;
+	}
+
+	public String getErro() {
+		return erro;
+	}
+	
+}
